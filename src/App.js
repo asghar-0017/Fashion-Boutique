7c13203f578485d508bf49273b6ca7d3cafb9600
Abdul Rehman <rehmankp186@gmail.com>
2024-10-12 23:22:06 +0500

    measurements

--- src/App.js
+++ src/App.js
@@ -1,161 +1,246 @@
 import { Suspense, lazy } from "react";
 import ScrollToTop from "./helpers/scroll-top";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 // home pages
-const HomeFashion = lazy(() => import("./pages/home/HomeFashion"));
-
-
-
-
-
-
+const HomeFashion = lazy(() =>
+  import("./pages/home/HomeFashion")
+);
 
 // shop pages
 
-const ShopGridNoSidebar = lazy(() => import("./pages/shop/ShopGridNoSidebar"));
-
+const ShopGridNoSidebar = lazy(() =>
+  import("./pages/shop/ShopGridNoSidebar")
+);
 
 // product pages
-const Product = lazy(() => import("./pages/shop-product/Product"));
+const Product = lazy(() =>
+  import("./pages/shop-product/Product")
+);
 const ProductTabLeft = lazy(() =>
   import("./pages/shop-product/ProductTabLeft")
 );
 const ProductTabRight = lazy(() =>
   import("./pages/shop-product/ProductTabRight")
 );
-const ProductSticky = lazy(() => import("./pages/shop-product/ProductSticky"));
-const ProductSlider = lazy(() => import("./pages/shop-product/ProductSlider"));
+const ProductSticky = lazy(() =>
+  import("./pages/shop-product/ProductSticky")
+);
+const ProductSlider = lazy(() =>
+  import("./pages/shop-product/ProductSlider")
+);
 const ProductFixedImage = lazy(() =>
   import("./pages/shop-product/ProductFixedImage")
 );
 
 // blog pages
-const BlogNoSidebar = lazy(() => import("./pages/blog/BlogNoSidebar"));
-
+const BlogNoSidebar = lazy(() =>
+  import("./pages/blog/BlogNoSidebar")
+);
 
 // other pages
-const About = lazy(() => import("./pages/other/About"));
-const Contact = lazy(() => import("./pages/other/Contact"));
-const MyAccount = lazy(() => import("./pages/other/MyAccount"));
-const LoginRegister = lazy(() => import("./pages/other/LoginRegister"));
+const About = lazy(() =>
+  import("./pages/other/About")
+);
+const Contact = lazy(() =>
+  import("./pages/other/Contact")
+);
+const MyAccount = lazy(() =>
+  import("./pages/other/MyAccount")
+);
+const LoginRegister = lazy(() =>
+  import("./pages/other/LoginRegister")
+);
 
-const Cart = lazy(() => import("./pages/other/Cart"));
-const Wishlist = lazy(() => import("./pages/other/Wishlist"));
-const Compare = lazy(() => import("./pages/other/Compare"));
-const Checkout = lazy(() => import("./pages/other/Checkout"));
+const Cart = lazy(() =>
+  import("./pages/other/Cart")
+);
+const Wishlist = lazy(() =>
+  import("./pages/other/Wishlist")
+);
+const Compare = lazy(() =>
+  import("./pages/other/Compare")
+);
+const Checkout = lazy(() =>
+  import("./pages/other/Checkout")
+);
+const Measurements = lazy(() =>
+  import("./pages/measurements/measurements")
+);
 
-const NotFound = lazy(() => import("./pages/other/NotFound"));
+const NotFound = lazy(() =>
+  import("./pages/other/NotFound")
+);
 
 const App = () => {
   return (
-      <Router>
-        <ScrollToTop>
-          <Suspense
-            fallback={
-              <div className="flone-preloader-wrapper">
-                <div className="flone-preloader">
-                  <span></span>
-                  <span></span>
-                </div>
+    <Router>
+      <ScrollToTop>
+        <Suspense
+          fallback={
+            <div className="flone-preloader-wrapper">
+              <div className="flone-preloader">
+                <span></span>
+                <span></span>
               </div>
-            }
-          >
-            <Routes>
-              <Route
-                path={process.env.PUBLIC_URL + "/"}
-                element={<HomeFashion/>}
-              />
+            </div>
+          }>
+          <Routes>
+            <Route
+              path={process.env.PUBLIC_URL + "/"}
+              element={<HomeFashion />}
+            />
 
-              {/* Homepages */}
-              <Route
-                path={process.env.PUBLIC_URL + "/home-fashion"}
-                element={<HomeFashion/>}
-              />
-              
+            {/* Homepages */}
+            <Route
+              path={
+                process.env.PUBLIC_URL +
+                "/home-fashion"
+              }
+              element={<HomeFashion />}
+            />
 
-              {/* Shop pages */}
-              
-              <Route
-                path={process.env.PUBLIC_URL + "/shop-grid-no-sidebar"}
-                element={<ShopGridNoSidebar/>}
-              />
-              
+            <Route
+              path={
+                process.env.PUBLIC_URL +
+                "/measurements"
+              }
+              element={<Measurements />}
+            />
 
-              {/* Shop product pages */}
-              <Route
-                path={process.env.PUBLIC_URL + "/product/:id"}
-                element={<Product />}
-              />
-              <Route
-                path={process.env.PUBLIC_URL + "/product-tab-left/:id"}
-                element={<ProductTabLeft/>}
-              />
-              <Route
-                path={process.env.PUBLIC_URL + "/product-tab-right/:id"}
-                element={<ProductTabRight/>}
-              />
-              <Route
-                path={process.env.PUBLIC_URL + "/product-sticky/:id"}
-                element={<ProductSticky/>}
-              />
-              <Route
-                path={process.env.PUBLIC_URL + "/product-slider/:id"}
-                element={<ProductSlider/>}
-              />
-              <Route
-                path={process.env.PUBLIC_URL + "/product-fixed-image/:id"}
-                element={<ProductFixedImage/>}
-              /> 
+            {/* Shop pages */}
 
-              {/* Blog pages */}
-              
-              <Route
-                path={process.env.PUBLIC_URL + "/blog-no-sidebar"}
-                element={<BlogNoSidebar/>}
-              />
-               
+            <Route
+              path={
+                process.env.PUBLIC_URL +
+                "/shop-grid-no-sidebar"
+              }
+              element={<ShopGridNoSidebar />}
+            />
 
-              {/* Other pages */}
-              <Route
-                path={process.env.PUBLIC_URL + "/about"}
-                element={<About/>}
-              />
-              <Route
-                path={process.env.PUBLIC_URL + "/contact"}
-                element={<Contact/>}
-              />
-              <Route
-                path={process.env.PUBLIC_URL + "/my-account"}
-                element={<MyAccount/>}
-              />
-              <Route
-                path={process.env.PUBLIC_URL + "/login-register"}
-                element={<LoginRegister/>}
-              />
+            {/* Shop product pages */}
+            <Route
+              path={
+                process.env.PUBLIC_URL +
+                "/product/:id"
+              }
+              element={<Product />}
+            />
+            <Route
+              path={
+                process.env.PUBLIC_URL +
+                "/product-tab-left/:id"
+              }
+              element={<ProductTabLeft />}
+            />
+            <Route
+              path={
+                process.env.PUBLIC_URL +
+                "/product-tab-right/:id"
+              }
+              element={<ProductTabRight />}
+            />
+            <Route
+              path={
+                process.env.PUBLIC_URL +
+                "/product-sticky/:id"
+              }
+              element={<ProductSticky />}
+            />
+            <Route
+              path={
+                process.env.PUBLIC_URL +
+                "/product-slider/:id"
+              }
+              element={<ProductSlider />}
+            />
+            <Route
+              path={
+                process.env.PUBLIC_URL +
+                "/product-fixed-image/:id"
+              }
+              element={<ProductFixedImage />}
+            />
 
-              <Route
-                path={process.env.PUBLIC_URL + "/cart"}
-                element={<Cart/>}
-              />
-              <Route
-                path={process.env.PUBLIC_URL + "/wishlist"}
-                element={<Wishlist/>}
-              />
-              <Route
-                path={process.env.PUBLIC_URL + "/compare"}
-                element={<Compare/>}
-              />
-              <Route
-                path={process.env.PUBLIC_URL + "/checkout"}
-                element={<Checkout/>}
-              /> 
+            {/* Blog pages */}
 
-              <Route path="*" element={<NotFound/>} />
-            </Routes>
-          </Suspense>
-        </ScrollToTop>
-      </Router>
+            <Route
+              path={
+                process.env.PUBLIC_URL +
+                "/blog-no-sidebar"
+              }
+              element={<BlogNoSidebar />}
+            />
+
+            {/* Other pages */}
+            <Route
+              path={
+                process.env.PUBLIC_URL + "/about"
+              }
+              element={<About />}
+            />
+            <Route
+              path={
+                process.env.PUBLIC_URL +
+                "/contact"
+              }
+              element={<Contact />}
+            />
+            <Route
+              path={
+                process.env.PUBLIC_URL +
+                "/my-account"
+              }
+              element={<MyAccount />}
+            />
+            <Route
+              path={
+                process.env.PUBLIC_URL +
+                "/login-register"
+              }
+              element={<LoginRegister />}
+            />
+
+            <Route
+              path={
+                process.env.PUBLIC_URL + "/cart"
+              }
+              element={<Cart />}
+            />
+            <Route
+              path={
+                process.env.PUBLIC_URL +
+                "/wishlist"
+              }
+              element={<Wishlist />}
+            />
+            <Route
+              path={
+                process.env.PUBLIC_URL +
+                "/compare"
+              }
+              element={<Compare />}
+            />
+            <Route
+              path={
+                process.env.PUBLIC_URL +
+                "/checkout"
+              }
+              element={<Checkout />}
+            />
+
+            <Route
+              path="*"
+              element={<NotFound />}
+            />
+          </Routes>
+        </Suspense>
+      </ScrollToTop>
+    </Router>
   );
 };
 
